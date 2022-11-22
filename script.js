af75ae1cc2accbfdc65d4898e0b607b580a0174f
qquliyevvv <quluzade228@gmail.com>
2022-11-22 15:43:49 +0400

    Toqrul10

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ let options = document.querySelector('.options');
 let buttons = document.querySelector('.buttons');
 let image = document.querySelector('.image');
 let options2 = document.querySelector('.options-2');
-const rub = document.querySelector('.rub');
-const usd = document.querySelector('.usd');
-const eur = document.querySelector('.eur');
-const azn = document.querySelector('.azn');
+const input1= document.querySelector('.input1');
+const input2= document.querySelector('.input2');
 const alltable = document.querySelectorAll('.alltable');
 const leftable = document.querySelector('.left-table');
 const rightable = document.querySelector('.right-table');
 const lth = document.querySelectorAll('.left-th');
 const p1 = document.querySelector('.p1');
 const p2 = document.querySelector('.p2');
-
+let koeficent = 0;
+let koeficent2=0;
 
 let value1;
 let value2;
@@ -56,7 +55,8 @@ function convert(val1, val2) {
       console.log(val1);
       console.log(data.rates[val2]);
       p1.innerHTML=`1 ${val1}=${data.rates[val2]} ${val2}`;
-      p2.innerHTML=`1 ${val2}=${data.rates[val1]} ${val1}`
+      koeficent = data.rates[val2];
+      // p2.innerHTML=`1 ${val2}=${data.rates[val1]} ${val1}`
     })
     fetch(`https://api.exchangerate.host/latest?base=${val2}&symbols=${val1}`)
     .then(response => response.json())
@@ -64,5 +64,14 @@ function convert(val1, val2) {
       console.log(val2);
       console.log(data.rates[val1]);
       p2.innerHTML=`1 ${val2}=${data.rates[val1]} ${val1}`
+      koeficent2 = data.rates[val1];
     })
 }
+input1.addEventListener('keyup',()=>{
+  input2.value = input1.value * koeficent;
+})
+input2.addEventListener('keyup',()=>{
+  input1.value = input2.value * koeficent2;
+})
+
+
